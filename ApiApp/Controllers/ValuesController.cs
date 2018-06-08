a6d8c2d356a86e074b5af9f1c2ba19e8af253bc4
fangh <fangh@LAPTOP-RLV91I3R>
2018-06-08 09:41:36 +0200

    added json ajax index

--- ApiApp/Controllers/ValuesController.cs
+++ ApiApp/Controllers/ValuesController.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web.Http;
 
 namespace ApiApp.Controllers
 {
     public class ValuesController : ApiController
     {
+
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "value");
+            response.Content = new StringContent("hello Fang", Encoding.Unicode);
+            response.Headers.CacheControl = new CacheControlHeaderValue()
+            {
+                MaxAge = TimeSpan.FromMinutes(20)
+            };
+            return response;
+        }
+
+
         static List<string> strings = new List<string>()
         {
             "id",
@@ -17,10 +32,10 @@ namespace ApiApp.Controllers
             "NrOfOrder"
         };
         // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return strings;
-        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return strings;
+        //}
 
         // GET api/values/5
         public string Get(int id)
